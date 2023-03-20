19dcb9e9703d21afca593f7d4b0fdd1a48ecb38d
PedroMaiaF <pedromaiaf21@gmail.com>
2023-03-19 20:14:29 -0400

    SRC-Controller adicionado o Comment Controller e refatorado o Post Controller e User Controller

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -1,24 +1,47 @@
-import { Request, Response } from "express"
-import { UserBusiness } from "../business/UserBusiness"
-import { LoginInputDTO, SignupInputDTO, SignupOutputDTO } from "../dtos/UserDTO"
-import { BaseError } from "../errors/BaseError"
+import { Request, Response } from "express";
+import { UserBusiness } from "../business/UserBusiness";
+import { CreateUserInputDTO, LoginInputDTO } from "../dtos/userDTO";
+import { BaseError } from "../errors/BaseError";
 
 export class UserController {
     constructor(
         private userBusiness: UserBusiness
-    ) {}
+    ){}
 
-    public signup = async (req: Request, res: Response) => {
+    public getUsers = async (req: Request, res: Response) => {
         try {
-            const input: SignupInputDTO = {
-                name: req.body.name,
+            const input = {
+                q: req.query.q,
+                token: req.headers.authorization
+            }
+
+            const output = await this.userBusiness.getUsers(input)
+
+            
+            res.status(200).send(output)
+
+        } catch (error) {
+            console.log(error)
+            if (error instanceof BaseError) {
+                res.status(error.statusCode).send(error.message)
+            } else {
+                res.status(500).send("Erro inesperado")
+            }
+        }
+    }
+
+    public createUser = async (req: Request, res: Response) => {
+        try {
+            const input: CreateUserInputDTO = {
+                nickName: req.body.nickName,
                 email: req.body.email,
                 password: req.body.password
             }
+        
+        const output = await this.userBusiness.createUser(input)
+        
+        res.status(201).send(output)
 
-            const output = await this.userBusiness.signup(input)
-
-            res.status(201).send(output)
         } catch (error) {
             console.log(error)
             if (error instanceof BaseError) {
@@ -35,10 +58,11 @@ export class UserController {
                 email: req.body.email,
                 password: req.body.password
             }
+        
+        const output = await this.userBusiness.login(input)
+        
+        res.status(200).send(output)
 
-            const output = await this.userBusiness.login(input)
-
-            res.status(200).send(output)
         } catch (error) {
             console.log(error)
             if (error instanceof BaseError) {
@@ -48,4 +72,7 @@ export class UserController {
             }
         }
     }
+
+
+
 }
